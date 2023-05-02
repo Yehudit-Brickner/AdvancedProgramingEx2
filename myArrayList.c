e697f5fc60dc3a77fa01b97795dab0f2b57c0176
Yehudit-Brickner <79203360+Yehudit-Brickner@users.noreply.github.com>
2023-05-02 09:38:00 +0300

    Add files via upload

--- myArrayList.c
+++ myArrayList.c
@@ -5,7 +5,10 @@
 #define INITIAL_CAPACITY 10
 
 typedef struct item {
-    char *data;
+    char *input;
+    char *output;
+    int index;
+    struct item *next;
 } item;
 
 typedef struct {
@@ -20,18 +23,18 @@ void init(arraylist *al) {
     al->array = malloc(al->capacity * sizeof(item));
 }
 
-void add(arraylist *al, const char *str) {
+void add(arraylist *al, item *it) {
     if (al->size == al->capacity) {
         al->capacity *= 2;
         al->array = realloc(al->array, al->capacity * sizeof(item));
     }
-    item *new_item = malloc(sizeof(item));
-    new_item->data = malloc(strlen(str) + 1);
-    strcpy(new_item->data, str);
-    al->array[al->size++] = *new_item;
+    // item *new_item = malloc(sizeof(item));
+    // new_item->data = malloc(strlen(str) + 1);
+    // strcpy(new_item->data, str);
+    al->array[al->size++] = *it;
 }
 
-void add_at(arraylist *al, const item *currItem, size_t index) {
+void add_at(arraylist *al, item *currItem, size_t index) {
     if (index > al->size) {
         // Resize the arraylist to accommodate the given index
         size_t new_capacity = index + 1;
@@ -55,6 +58,7 @@ item* get(arraylist *al, size_t index) {
         printf("Error: Index out of bounds\n");
         return NULL;
     }
+    // printf("%s",&al->array[index].output);
     return &al->array[index];
 }
 
@@ -64,7 +68,7 @@ void remove_item(arraylist *al, size_t index) {
         return;
     }
     item *item_to_remove = &al->array[index];
-    free(item_to_remove->data);
+    // free(item_to_remove->data);
     free(item_to_remove);
     memmove(&al->array[index], &al->array[index + 1], (al->size - index - 1) * sizeof(item));
     al->size--;
@@ -72,7 +76,7 @@ void remove_item(arraylist *al, size_t index) {
 
 void destroy(arraylist *al) {
     for (size_t i = 0; i < al->size; i++) {
-        free(al->array[i].data);
+        // free(al->array[i].data);
         free(&al->array[i]);
     }
     free(al->array);
@@ -81,16 +85,38 @@ void destroy(arraylist *al) {
 int main() {
     arraylist al;
     init(&al);
-    add(&al, "Hello");
-    add(&al, "World");
-    add_at(&al, "there", 1);
-    item* i = get(&al, 2);
-    printf("Got item: %s\n", i->data);
+    printf("created arraylist\n");
+    
+    item *i0;
+    i0->input="world";
+    i0->output="wprld";
+    i0->index=0;
+    i0->next=NULL;
+    item *i1;
+    i1->input="hello";
+    i1->output="hello";
+    i1->index=1;
+    i1->next=NULL;
+    item *i2;
+    i2->input="yehudit";
+    i2->output="tehudit";
+    i2->index=2;
+    i2->next=NULL;
+    
+    printf("created items\n");
+
+
+    add_at(&al, i0, 0);
+    add_at(&al, i1, 1);
+    add_at(&al, i2, 3);
+
+    item* i = get(&al, 1);
+    
     remove_item(&al, 0);
     printf("Removed item 0\n");
-    for (size_t j = 0; j < al.size; j++) {
-        printf("%s ", al.array[j].data);
-    }
+    // for (size_t j = 0; j < al.size; j++) {
+    //     printf("%s ", al.array[j].data);
+    // }
     printf("\n");
     destroy(&al);
     return 0;
